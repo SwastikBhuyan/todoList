06ba3f70276c062e544b74d7931db433caa39693
SwastikBhuyan <154882312+SwastikBhuyan@users.noreply.github.com>
2024-07-19 17:32:38 +0530

    Added footer component

--- todo-list-frontend/src/App.js
+++ todo-list-frontend/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import HeaderComponent from "./components/HeaderComponent";
+import FooterComponent from "./components/FooterComponent";
 
 function App() {
     return (
         <div className="App">
             <HeaderComponent />
+            <FooterComponent />
         </div>
     );
 }
---
+++ todo-list-frontend/src/components/FooterComponent.jsx
@@ -0,0 +1,9 @@
+export default function FooterComponent() {
+    return (
+        <footer className="footer bg-dark text-white text-center py-3">
+            <div className="container">
+                <span>2024 Swastik Bhuyan. All rights reserved.</span>
+            </div>
+        </footer>
+    );
+}
